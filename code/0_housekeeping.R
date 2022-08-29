bbce885c8695816e64e3a04037ca060cd03dfb94
Karen Hotopp <karen.hotopp3@phs.scot>
2022-08-29 17:58:04 +0100

    Adding changes to work with current process.

--- code/0_housekeeping.R
+++ code/0_housekeeping.R
@@ -15,8 +15,9 @@ wd <-paste0("/PHI_conf/CancerGroup1/Topics/BreastScreening/Investigations",
 root <- paste0("/PHI_conf/CancerGroup1/Topics/BreastScreening/Investigations",
                "/20201203-Breast-Screening-NSOB-Restart-Metrics",
                "/Weekly Appts Reports/Invite Screen Reports/R079")
-fname <- "/Scotland_R079_20"
 
+file_name <- "/Scotland_R079_"
+YYMM <- 202207 # current year and month to add to data
 today <- Sys.Date()
 
 
