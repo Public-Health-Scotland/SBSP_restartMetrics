9d824eec157addda79d581498094410f039cbf89
Karen Hotopp <karen.hotopp3@phs.scot>
2022-08-29 17:45:08 +0100

    Combining processes for creating historical database of R079. To create new main script for monthly updates.

--- code/x_R079_import_historic.R
+++ code/x_R079_import_historic.R
@@ -1,8 +1,8 @@
 ##########################################################
-# x_R079_import_historic.R
+# x_R079_recreate_historic.R
 # Karen Hotopp
 # 26/08/2022
-# Script x1 of 2
+# Script x1 of ?
 # Pulling in historic R079 reports to create a complete historic file
 # that uses downloaded R079 reports (vs weekly sent reports)
 # Completed data file starts at Jan 2018
@@ -268,3 +268,307 @@ range(complete$WorklistDate)
 
 ## Export
 write_rds(complete, paste0(wd, "/Output/SBSS_R079_historic_", today, ".rds"))
+
+
+
+##########################################################
+# x_R079_create_historic.R
+# Karen Hotopp
+# 26/08/2022
+# Script x1 of 2
+# Call in historic R079 RDS file and create counts of appointments
+# allocated v attended
+# Completed data file starts at Jan 2018
+# Written/run on R Studio Server
+# R version 3.6.1
+##########################################################
+
+
+#### 1: Housekeeping ####
+## Packages
+library(dplyr)
+library(magrittr)
+library(lubridate)
+library(forcats)
+library(tidylog)
+
+
+## Pathways
+rm(list = ls())
+wd <-paste0("/PHI_conf/CancerGroup1/Topics/BreastScreening/Investigations",
+            "/20201203-Breast-Screening-NSOB-Restart-Metrics")
+source(paste0(wd, "/SBSP_restartMetrics/code/0_housekeeping.R"))
+
+
+#### 2: Import data ####
+brest <- readRDS(paste0(wd, "/Output/SBSS_R079_historic_2022-08-26.rds"))
+
+## Define start date for each month -- this is just 1st of the month
+brest %<>%
+  mutate(start_date = floor_date(as_date(WorklistDate), "month")) %>% 
+  glimpse()
+
+
+#### 3: Set up counts ####
+## Create month counts by centres
+# There are 55 months, so each centre should have 55 records
+full_db <- brest %>%
+  group_by(BSCName, start_date) %>%
+  summarize(Allocated = sum(Allocated),
+            Attended = sum(Attended)) %>%
+  ungroup
+
+full_db_scot <- brest %>%
+  group_by(start_date) %>%
+  summarize(Allocated = sum(Allocated),
+            Attended = sum(Attended)) %>%
+  ungroup() %>% 
+  mutate(BSCName = "Scotland", .before = start_date)
+
+full_db <- bind_rows(full_db, full_db_scot)
+View(full_db)
+
+
+#### What are the missing months?
+table(brest$BSCName)
+east <- brest[brest$BSCName == "East of Scotland",]
+noreast <- brest[brest$BSCName == "North East of Scotland",]
+north <- brest[brest$BSCName == "North of Scotland",]
+soeast <- brest[brest$BSCName == "South East of Scotland",]
+sowest <- brest[brest$BSCName == "South West of Scotland",]
+west <- brest[brest$BSCName == "West of Scotland",]
+
+table(east$start_date) # missing May/June 2020
+table(noreast$start_date) # missing June/July 2020
+table(soeast$start_date) # missing May/June/July 2020
+table(sowest$start_date) # missing July 2020
+table(west$start_date) # missing May/June 2020
+# These centres have no records for the months above (COVID-19 program stop)
+
+rm(east, noreast, soeast, sowest, west)
+
+
+## Restructure db
+full_db %<>%
+  mutate(month = format(start_date, "%h-%Y"), .after=BSCName) %>%
+  mutate(BSCName = factor(BSCName)) %>% 
+  mutate(BSCName = fct_relevel(BSCName, c("East of Scotland",
+                                          "North East of Scotland",
+                                          "North of Scotland",
+                                          "South East of Scotland",
+                                          "South West of Scotland",
+                                          "West of Scotland",
+                                          "Scotland"))) %>% 
+  arrange(BSCName, start_date) %>% 
+  glimpse()
+
+
+## Separate out two counts  
+## Allocated
+allocated <- full_db %>%
+  select(BSCName, month, Allocated) %>% 
+  pivot_wider(names_from = month, values_from = Allocated) %>% 
+  mutate(appt_type = "allocated", .after = BSCName) %>% 
+  relocate(`May-2020`, `Jun-2020`, .after = `Apr-2020`) %>% 
+  glimpse()
+# replce all NAs with 0
+allocated[is.na(allocated)] <- 0
+
+## Attended
+attended <- full_db %>%
+  select(BSCName, month, Attended) %>% 
+  pivot_wider(names_from = month, values_from = Attended) %>% 
+  mutate(appt_type = "attended", .after = BSCName) %>% 
+  relocate(`May-2020`, `Jun-2020`, .after = `Apr-2020`) %>% 
+  glimpse()
+# replce all NAs with 0
+attended[is.na(attended)] <- 0
+
+
+## Create totals counts
+sum <- full_db %>% 
+  group_by(BSCName) %>% 
+  summarize(Allocated = sum(Allocated),
+            Attended = sum(Attended)) %>%
+  ungroup %>% 
+  rename(total_alloc = Allocated,
+         total_attend = Attended) %>% 
+  glimpse
+
+
+## Combine counts and totals
+sum_all <- select(sum, BSCName, total_alloc) %>% 
+  rename(Total = total_alloc)
+sum_att <- select(sum, BSCName, total_attend) %>% 
+  rename(Total = total_attend)
+
+allocated <- left_join(allocated, sum_all)
+attended <- left_join(attended, sum_att)
+
+
+#### 4: Rejoin and Save ####
+## Rejoin counts by stacking
+full_metrics <- bind_rows(allocated, attended)
+
+## Save
+saveRDS(full_metrics, paste0(wd, "/Output/SBSS_R079_historic.rds"))
+
+
+
+##########################################################
+# x3_R079_write_Excel.R
+# Karen Hotopp
+# 29/08/2022
+# Script x3 of ?
+# Call in historic R079 RDS file and create counts of appointments
+# allocated v attended
+# Completed data file starts at Jan 2018
+# Written/run on R Studio Server
+# R version 3.6.1
+# **This is a scratch pad for writing out to Excel and will be 
+# deleted/incorporated into the processing file!**
+##########################################################
+
+
+#### 1: Housekeeping ####
+## Packages
+library(dplyr)
+library(magrittr)
+library(openxlsx)
+library(tidylog)
+
+
+## Pathways
+rm(list = ls())
+wd <-paste0("/PHI_conf/CancerGroup1/Topics/BreastScreening/Investigations",
+            "/20201203-Breast-Screening-NSOB-Restart-Metrics")
+
+today <- Sys.Date()
+
+
+## Data
+hist <- readRDS(paste0(wd, "/Output/SBSS_R079_historic.rds"))
+
+
+#### 2: Set up data for Excel ####
+## Br - Attendances tab
+## Historical average (2018/19) ---
+hist_1819 <- hist %>% 
+  filter(BSCName == "Scotland") %>% 
+  select(BSCName:`Dec-2019`) %>% 
+  glimpse()
+
+hist_1819 %<>% 
+  # find monthly average for the two years
+  # Excel file starts at Aug, then loops continuously
+  group_by(BSCName, appt_type) %>% 
+  summarize(`Aug 18/19` = (`Aug-2018` + `Aug-2019`)/2,
+            `Sep 18/19` = (`Sep-2018` + `Sep-2019`)/2,
+            `Oct 18/19` = (`Oct-2018` + `Oct-2019`)/2,
+            `Nov 18/19` = (`Nov-2018` + `Nov-2019`)/2,
+            `Dec 18/19` = (`Dec-2018` + `Dec-2019`)/2,
+            `Jan 18/19` = (`Jan-2018` + `Jan-2019`)/2,
+            `Feb 18/19` = (`Feb-2018` + `Feb-2019`)/2,
+            `Mar 18/19` = (`Mar-2018` + `Mar-2019`)/2,
+            `Apr 18/19` = (`Apr-2018` + `Apr-2019`)/2,
+            `May 18/19` = (`May-2018` + `May-2019`)/2,
+            `Jun 18/19` = (`Jun-2018` + `Jun-2019`)/2,
+            `Jul 18/19` = (`Jul-2018` + `Jul-2019`)/2) %>% 
+  ungroup() %>% 
+  glimpse()
+
+# Full month historic
+hist1819_full <- hist_1819 %>% 
+  select(-c(BSCName:appt_type))
+# # Active month historic
+# # add months (selecting columns) to this as appropriate to extend historic loop
+# hist1819_active <- hist_1819 %>% 
+#   select(`Aug 18/19`)
+
+
+### July 2020 to current ----
+## Br - Attendances tab
+## Scotland
+scot <- hist %>%
+  filter(BSCName == "Scotland") %>% 
+  select(-c(BSCName:`Jul-2020`)) %>% 
+  glimpse
+
+## Centres--Allocated
+allocated <- hist %>%
+  filter(BSCName != "Scotland",
+         appt_type == "allocated") %>%
+  select(-c(BSCName:`Jul-2020`)) %>% 
+  glimpse
+
+## Centres--Attended
+attended <- hist %>%
+  filter(BSCName != "Scotland",
+         appt_type == "attended") %>%
+  select(-c(BSCName:`Jul-2020`)) %>% 
+  glimpse
+
+
+### January 2018 to Mar 2020 ----
+## Br - Historical Attendances tab
+## Scotland
+scot_hist <- hist %>%
+  filter(BSCName == "Scotland") %>% 
+  select(c(`Jan-2018`:`Mar-2020`)) %>% 
+  glimpse
+
+## Centres--Allocated
+allocated_hist <- hist %>%
+  filter(BSCName != "Scotland",
+         appt_type == "allocated") %>%
+  select(c(`Jan-2018`:`Mar-2020`)) %>% 
+  glimpse
+
+## Centres--Attended
+attended_hist <- hist %>%
+  filter(BSCName != "Scotland",
+         appt_type == "attended") %>%
+  select(c(`Jan-2018`:`Mar-2020`)) %>% 
+  glimpse
+
+
+#### 3: Write to Excel ####
+wb <- loadWorkbook(paste0(wd, "/Output/NSOB Recovery Metrics Breast Screening_KHtest.xlsx"))
+
+### July 2020 to current ----
+writeData(wb, sheet = "Br - Attendances", scot, 
+          startCol = "B", startRow = 1, colNames = T)
+writeData(wb, sheet = "Br - Attendances", allocated, 
+          startCol = "B", startRow = 13, colNames = T)
+writeData(wb, sheet = "Br - Attendances", attended, 
+          startCol = "B", startRow = 22, colNames = T)
+
+### Historical average (2018/19) ---
+writeData(wb, sheet = "Br - Attendances", hist1819_full, 
+          startCol = "B", startRow = 7, colNames = T)
+writeData(wb, sheet = "Br - Attendances", hist1819_full, 
+          startCol = "N", startRow = 7, colNames = T)
+# uncomment below to add historic loop months to historic average (2018/19)
+# writeData(wb, sheet = "Br - Attendances", hist1819_active, 
+#           startCol = "Z", startRow = 7, colNames = T)
+
+writeData(wb, sheet = "Br - Attendances", paste0("Updated ", today),
+          startCol = "A", startRow = 42)
+
+
+### January 2018 to Mar 2020 ----
+writeData(wb, sheet = "Br - Historical Attendances", scot_hist, 
+          startCol = "B", startRow = 1, colNames = T)
+writeData(wb, sheet = "Br - Historical Attendances", allocated_hist, 
+          startCol = "B", startRow = 6, colNames = T)
+writeData(wb, sheet = "Br - Historical Attendances", attended_hist, 
+          startCol = "B", startRow = 15, colNames = T)
+
+writeData(wb, sheet = "Br - Attendances", paste0("Updated ", today),
+          startCol = "A", startRow = 26)
+
+
+### Save ----
+saveWorkbook(wb, paste0(wd, 
+                        "/Output/NSOB Recovery Metrics Breast Screening_DidItWork.xlsx"),
+             overwrite = T)
